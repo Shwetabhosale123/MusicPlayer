204a7f7ac22b7e36f396e491a1f3080fc3caca7e
shweta bhosale <shwetabhosale30@gmail.com>
2024-03-01 18:09:16 +0530

    MusicPlayer Project

---
+++ app/src/main/java/com/example/musicplayer/MusicAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.musicplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.musicplayer.databinding.MusicViewBinding
+
+class MusicAdapter(private val context:Context,private val musicList:ArrayList<Music>) : RecyclerView.Adapter<MusicAdapter.MyHolder>(){
+
+    class MyHolder(binding: MusicViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        val title=binding.songNameMv
+        val album=binding.songAlbumMV
+        val image=binding.imageMV
+        val duration=binding.songDuration
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
+       return MyHolder(MusicViewBinding.inflate(LayoutInflater.from(context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
+        holder.title.text=musicList[position].title
+        holder.album.text=musicList[position].album
+        holder.duration.text=musicList[position].duration.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return musicList.size
+    }
+
+}
---
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.kt
@@ -0,0 +1,15 @@
+package com.example.musicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicplayer.databinding.ActivityPlayerBinding
+
+class PlayerActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityPlayerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.coolPink)
+        binding=ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
